bc7490e78e3e0da792aefdd691bb0ca4ab24a669
akarahdev <118059229+akarahdev@users.noreply.github.com>
2023-07-21 13:01:37 -0400

    add benchmarks for hashbrown

---
+++ examples/vs_hashbrown.rs
@@ -0,0 +1,31 @@
+use hashbrown::HashMap;
+
+use string_table::structure::StringTable;
+
+fn main() {
+    for counter in 1..5 {
+        println!("Run {counter};");
+        println!("Benchmarking HashMap<String, i32>");
+        let now = std::time::Instant::now();
+        {
+            let mut map = HashMap::new();
+            for x in 1..1000000 {
+                map.insert(format!("{x}"), x);
+            }
+        }
+        let end = now.elapsed();
+        println!("Time taken: {end:?}");
+        
+        println!("Benchmarking StringTable<i32>");
+        let now = std::time::Instant::now();
+        {
+            let mut table = StringTable::new();
+            for x in 1..1000000 {
+                table.insert(&format!("{x}"), x);
+            }
+        }
+        let end = now.elapsed();
+        println!("Time taken: {end:?}");
+        println!();
+    }
+}
